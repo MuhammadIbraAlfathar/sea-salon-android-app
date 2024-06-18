02d91140ddf260bf94eccbe6105d76f1c0b6866a
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-18 13:16:45 +0700

    first commit

---
+++ app/src/main/java/com/example/seasalonapp/presentation/activity/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.seasalonapp.presentation.activity.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seasalonapp.R
+import com.example.seasalonapp.presentation.activity.auth.RegisterActivity
+
+@SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 3000)
+    }
+}
