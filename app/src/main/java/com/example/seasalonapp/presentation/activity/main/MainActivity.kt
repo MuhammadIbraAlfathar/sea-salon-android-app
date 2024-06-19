f869cdffa924d6985a10f698dae9f2b18dedbb93
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-19 14:32:23 +0700

    add history fragment in bottom nav

--- app/src/main/java/com/example/seasalonapp/presentation/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/seasalonapp/presentation/activity/main/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_ratings, R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_ratings, R.id.navigation_profile, R.id.navigation_history
             )
         )
 
